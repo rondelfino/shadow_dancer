50437f3a68abe9bd6e35596b54eb4ca736c1e912
Nael Iskandarani <nael.nanomano@gmail.com>
2022-09-13 19:03:05 -0500

    Move constants to constants.rs

--- src/constants.rs
+++ src/constants.rs
@@ -1,7 +1,7 @@
-pub const WORLD_WIDTH: f32 = 800.0;
-pub const WORLD_HEIGHT: f32 = 450.0;
+pub const WORLD_WIDTH: f32 = 600.0;
+pub const WORLD_HEIGHT: f32 = 337.5;
 
-pub const LEFT_WALL: f32 = (-WORLD_WIDTH + 300.0) / 2.0;
-pub const RIGHT_WALL: f32 = (WORLD_WIDTH - 300.0) / 2.0;
+pub const LEFT_WALL: f32 = (-WORLD_WIDTH / 2.0) + 48.0;
+pub const RIGHT_WALL: f32 = (WORLD_WIDTH / 2.0) - 48.0;
 
 pub const PLAYER_SPEED: f32 = 600.0;
