3480df006cfee5520bfc8c6c63ff8e446c28af5a
Nael Iskandarani <nael.nanomano@gmail.com>
2022-12-06 18:58:00 -0600

    Move general functions to utils.rs

---
+++ src/utils.rs
@@ -0,0 +1,44 @@
+use bevy::prelude::*;
+
+use crate::components::MarkDespawn;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, SystemLabel)]
+pub enum GameSystemLabel {
+    Core,
+    Cleanup,
+}
+
+#[derive(Clone, Eq, PartialEq, Debug, Hash)]
+pub enum GameState {
+    Splash,
+    MainMenu,
+    StageIntro,
+    InGame,
+    Paused,
+}
+pub struct Bounds {
+    pub top: f32,
+    pub right: f32,
+    pub bottom: f32,
+    pub left: f32,
+}
+
+pub fn calculate_bounds(transform: &Transform, size: Option<Vec2>) -> Bounds {
+    let left_bound = transform.translation.x - size.unwrap_or_default().x / 2.0;
+    let right_bound = transform.translation.x + size.unwrap_or_default().x / 2.0;
+    let top_bound = transform.translation.y + size.unwrap_or_default().y / 2.0;
+    let bottom_bound = transform.translation.y - size.unwrap_or_default().y / 2.0;
+
+    Bounds {
+        top: top_bound,
+        right: right_bound,
+        bottom: bottom_bound,
+        left: left_bound,
+    }
+}
+
+pub fn despawner(mut commands: Commands, query: Query<Entity, With<MarkDespawn>>) {
+    for entity in query.iter() {
+        commands.entity(entity).despawn();
+    }
+}
