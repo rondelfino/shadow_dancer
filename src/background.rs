889b1c3657e4315255ffc5aba4c92fd517c383a8
Nael Iskandarani <nael.nanomano@gmail.com>
2022-12-09 23:27:12 -0600

    Edit background scale

--- src/background.rs
+++ src/background.rs
@@ -11,13 +11,13 @@ impl BackgroundBundle {
     pub fn new(texture_image_handle: Handle<Image>) -> Self {
         let dimensions = Dimensions(Vec2::new(1248.0, 1667.0));
 
-        let scale = WORLD_WIDTH / (dimensions.0.x - 260.0);
+        let scale = WORLD_WIDTH / (dimensions.0.x - 207.0);
         let sprite_bundle = SpriteBundle {
             texture: texture_image_handle,
             transform: Transform {
                 translation: Vec3::new(
                     0.0,
-                    -((dimensions.0.y * scale) / 2.0) + (WORLD_HEIGHT / 4.0) + 45.0,
+                    -((dimensions.0.y * scale) / 2.0) + (WORLD_HEIGHT / 4.0) + 55.0,
                     0.0,
                 ),
                 scale: Vec3::new(scale, scale, 0.0),
@@ -34,7 +34,7 @@ impl BackgroundBundle {
 }
 
 pub fn spawn_background(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let texture_image_handle: Handle<Image> = asset_server.load("background/skybox.png");
+    let texture_image_handle: Handle<Image> = asset_server.load("background/background_day.png");
 
     commands
         .spawn_empty()
