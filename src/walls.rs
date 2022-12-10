0eb4ccb329b340d5390caeb9d59ee3f78edf8111
Nael Iskandarani <nael.nanomano@gmail.com>
2022-12-09 23:23:57 -0600

    Spawn walls separately

--- src/walls.rs
+++ src/walls.rs
@@ -13,13 +13,13 @@ impl WallBundle {
         index: i32,
         left_roof_height: f32,
     ) -> Self {
-        let dimensions = Dimensions(Vec2::new(48.0, 224.0));
+        let dimensions = Dimensions(Vec2::new(48.0, 244.0));
 
-        let mut sprite_bundle = SpriteSheetBundle {
+        let sprite_bundle = SpriteSheetBundle {
             texture_atlas: texture_atlas_handle,
             transform: Transform {
                 translation: Vec3::new(
-                    LEFT_WALL - dimensions.0.x,
+                    LEFT_WALL - dimensions.0.x / 2.0,
                     (index as f32 * dimensions.0.y)
                         - (left_roof_height / 2.0 + dimensions.0.y / 2.0)
                         - 100.0,
@@ -29,8 +29,6 @@ impl WallBundle {
             },
             ..default()
         };
-
-        sprite_bundle.sprite.index = 0;
 
         WallBundle {
             wall: Wall,
@@ -44,13 +42,13 @@ impl WallBundle {
         index: i32,
         right_roof_height: f32,
     ) -> Self {
-        let dimensions = Dimensions(Vec2::new(48.0, 224.0));
+        let dimensions = Dimensions(Vec2::new(48.0, 244.0));
 
-        let mut sprite_bundle = SpriteSheetBundle {
+        let sprite_bundle = SpriteSheetBundle {
             texture_atlas: texture_atlas_handle,
             transform: Transform {
                 translation: Vec3::new(
-                    RIGHT_WALL + dimensions.0.x,
+                    RIGHT_WALL + dimensions.0.x / 2.0,
                     (index as f32 * dimensions.0.y)
                         - (right_roof_height / 2.0 + dimensions.0.y / 2.0)
                         - 100.0,
@@ -60,8 +58,6 @@ impl WallBundle {
             },
             ..default()
         };
-
-        sprite_bundle.sprite.index = 1;
 
         WallBundle {
             wall: Wall,
@@ -78,20 +74,37 @@ pub fn spawn_walls(
     left_roof_height: f32,
     right_roof_height: f32,
 ) {
-    let texture_handle = asset_server.load("objects/walls.png");
-    let texture_atlas =
-        TextureAtlas::from_grid(texture_handle, Vec2::new(48.0, 224.0), 2, 1, None, None);
-    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    let left_texture_handle = asset_server.load("background/left_wall.png");
+    let left_texture_atlas = TextureAtlas::from_grid(
+        left_texture_handle,
+        Vec2::new(48.0, 244.0),
+        2,
+        1,
+        None,
+        None,
+    );
+    let left_texture_atlas_handle = texture_atlases.add(left_texture_atlas);
+
+    let right_texture_handle = asset_server.load("background/right_wall.png");
+    let right_texture_atlas = TextureAtlas::from_grid(
+        right_texture_handle,
+        Vec2::new(48.0, 244.0),
+        2,
+        1,
+        None,
+        None,
+    );
+    let right_texture_atlas_handle = texture_atlases.add(right_texture_atlas);
 
     //spawns walls that span offscreen
     for i in -4..=0 {
         commands.spawn_empty().insert(WallBundle::left_wall(
-            texture_atlas_handle.clone(),
+            left_texture_atlas_handle.clone(),
             i,
             left_roof_height,
         ));
         commands.spawn_empty().insert(WallBundle::right_wall(
-            texture_atlas_handle.clone(),
+            right_texture_atlas_handle.clone(),
             i,
             right_roof_height,
         ));
