88ba09fecf0c1f90e15a14cc036006359082a938
Nael Iskandarani <nael.nanomano@gmail.com>
2022-12-09 18:32:18 -0600

    Edit constants

--- src/constants.rs
+++ src/constants.rs
@@ -3,11 +3,11 @@ pub const WORLD_HEIGHT: f32 = 800.0;
 
 pub const ASPECT_RATIO: f32 = WORLD_WIDTH / WORLD_HEIGHT;
 
-pub const LEFT_WALL: f32 = (-WORLD_WIDTH / 2.0) + 48.0;
-pub const RIGHT_WALL: f32 = (WORLD_WIDTH / 2.0) - 48.0;
+pub const LEFT_WALL: f32 = -WORLD_WIDTH / 2.0;
+pub const RIGHT_WALL: f32 = WORLD_WIDTH / 2.0;
 pub const UPPER_BOUND: f32 = WORLD_HEIGHT / 4.0 - 20.5;
 pub const LOWER_BOUND: f32 = -WORLD_HEIGHT / 4.0 + 20.5;
 
 pub const PLAYER_AIR_SPEED: f32 = 350.0;
-pub const FALLING_SPEED: f32 = 500.0;
+pub const FALLING_SPEED: f32 = 2000.0;
 pub const WALKING_SPEED: f32 = 150.0;
