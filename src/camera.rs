1a68c90445e0604428d06e418812b272c5c081bc
Nael Iskandarani <nael.nanomano@gmail.com>
2022-12-09 23:26:20 -0600

    Remove unused params

--- src/camera.rs
+++ src/camera.rs
@@ -3,18 +3,11 @@ use bevy::core_pipeline::clear_color::ClearColorConfig;
 use crate::prelude::*;
 
 pub fn camera_setup(mut commands: Commands) {
-    let half_height = WORLD_HEIGHT / 2.0;
-    let half_width = WORLD_WIDTH / 2.0;
-
     let camera_bundle = Camera2dBundle {
         projection: OrthographicProjection {
             far: 1000.0,
             scaling_mode: ScalingMode::FixedVertical(WORLD_HEIGHT),
-            scale: 0.55,
-            left: -half_width,
-            right: half_width,
-            top: half_height,
-            bottom: -half_height,
+            scale: CAMERA_SCALE,
             ..Default::default()
         },
         camera_2d: Camera2d {
