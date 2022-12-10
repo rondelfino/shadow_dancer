4504450cebb19e00666ff9eb42cd063fab5434aa
Nael Iskandarani <nael.nanomano@gmail.com>
2022-12-09 23:26:51 -0600

    Add camera scale

--- src/constants.rs
+++ src/constants.rs
@@ -1,13 +1,14 @@
-pub const WORLD_WIDTH: f32 = 500.0;
-pub const WORLD_HEIGHT: f32 = 800.0;
+pub const WORLD_WIDTH: f32 = 480.0;
+pub const WORLD_HEIGHT: f32 = 640.0;
 
 pub const ASPECT_RATIO: f32 = WORLD_WIDTH / WORLD_HEIGHT;
 
+pub const CAMERA_SCALE: f32 = 0.6;
 pub const LEFT_WALL: f32 = -WORLD_WIDTH / 2.0;
 pub const RIGHT_WALL: f32 = WORLD_WIDTH / 2.0;
-pub const UPPER_BOUND: f32 = WORLD_HEIGHT / 4.0 - 20.5;
-pub const LOWER_BOUND: f32 = -WORLD_HEIGHT / 4.0 + 20.5;
+pub const UPPER_BOUND: f32 = (WORLD_HEIGHT / 2.0) * CAMERA_SCALE - 20.5;
+pub const LOWER_BOUND: f32 = -(WORLD_HEIGHT / 2.0) * CAMERA_SCALE + 20.5;
 
 pub const PLAYER_AIR_SPEED: f32 = 350.0;
-pub const FALLING_SPEED: f32 = 2000.0;
+pub const FALLING_SPEED: f32 = 200.0;
 pub const WALKING_SPEED: f32 = 150.0;
