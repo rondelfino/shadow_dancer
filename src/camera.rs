3ce5047d9113395a6545243583f94f9fb023e70b
Nael Iskandarani <nael.nanomano@gmail.com>
2022-12-16 22:19:42 -0600

    Add camera plugin

--- src/camera.rs
+++ src/camera.rs
@@ -2,19 +2,30 @@ use bevy::core_pipeline::clear_color::ClearColorConfig;
 
 use crate::prelude::*;
 
-pub fn camera_setup(mut commands: Commands) {
-    let camera_bundle = Camera2dBundle {
-        projection: OrthographicProjection {
-            far: 1000.0,
-            scaling_mode: ScalingMode::FixedVertical(WORLD_HEIGHT),
-            scale: CAMERA_SCALE,
-            ..Default::default()
-        },
-        camera_2d: Camera2d {
-            clear_color: ClearColorConfig::Custom(Color::rgb(0.0, 0.0, 0.0)),
-        },
-        ..Default::default()
-    };
+pub struct CameraPlugin;
+impl Plugin for CameraPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_system_set(
+            SystemSet::on_enter(GameState::InGame).with_system(Self::camera_setup),
+        );
+    }
+}
 
-    commands.spawn(camera_bundle);
+impl CameraPlugin {
+    pub fn camera_setup(mut commands: Commands) {
+        let camera_bundle = Camera2dBundle {
+            projection: OrthographicProjection {
+                far: 1000.0,
+                scaling_mode: ScalingMode::FixedVertical(WORLD_HEIGHT),
+                scale: CAMERA_SCALE,
+                ..Default::default()
+            },
+            camera_2d: Camera2d {
+                clear_color: ClearColorConfig::Custom(Color::rgb(0.0, 0.0, 0.0)),
+            },
+            ..Default::default()
+        };
+
+        commands.spawn(camera_bundle);
+    }
 }
